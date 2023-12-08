3d01b4ea68d808623acafb362880973ff9a411ab
Arbaz Khan <124072249+arbazKhan33@users.noreply.github.com>
2023-12-08 16:06:56 -0600

    Add files via upload

---
+++ Services/Interfaces/EmailService.cs
@@ -0,0 +1,54 @@
+﻿using FitnessPro.ViewModel;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+using MailKit.Net.Smtp;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessPro.Services.Interfaces
+{
+    public class EmailService : IFitnessEmailSender
+    {
+        private readonly MailSettings _mailSettings;
+
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+        
+        
+        public Task sendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(_mailSettings.Mail));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder()
+
+            {
+                HtmlBody=htmlMessage
+            };
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+
+            await smtp.SendAsync(email);
+
+            smtp.Disconnect(true);
+
+            
+        }
+    }
+}
---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,18 @@
+﻿namespace FitnessPro.Services.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
+
+   // 1. services, dependency injection, and interfaces
+   // 2. create our IImageService interface
+   // 3. handle our incoming image file
+   // 4. add suffixes (size)
+   // 5. show a default image if none is uploaded
+   // 6. convert the file data
+   // 7. The image to the database
+   // 8. change the image in the view using JavaScript
+   // 9. register our IImageService
