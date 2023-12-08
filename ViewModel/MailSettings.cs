d31bc1f7f74bffae1cdd5cadaff4729a60f08978
Arbaz Khan <124072249+arbazKhan33@users.noreply.github.com>
2023-12-08 16:08:08 -0600

    Add files via upload

---
+++ ViewModel/MailSettings.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FitnessPro.ViewModel
+{
+    public class MailSettings
+    {
+        //configure and use smtp server from google 
+
+        public string? Mail { get; set; }
+        public string? DisplayNmae { get; set; }
+        public string? Password { get; set; }
+        public string? Host {get;set;}
+        public int Port { get; set; }
+    }
+}
